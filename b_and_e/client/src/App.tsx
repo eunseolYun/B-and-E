357554894cfa0708650b2cf584ad0bc0c01fd08b
Boyoung <boyoung.y589@gmail.com>
2023-04-19 16:09:51 +0900

    ADD: 초기설정

---
+++ b_and_e/client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from '../src/img/logo.png';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
