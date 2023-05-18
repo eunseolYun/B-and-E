6f8c338e434326997eca4b12ee2d1e0941b229ca
Boyoung <boyoung.y589@gmail.com>
2023-05-18 18:05:44 +0900

    ADD:server

--- b_and_e/client/src/component/Header.tsx
+++ b_and_e/client/src/component/Header.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
-import logo from '../img/logo.png';
+import logo from '../img/logo/logo.png';
 
 const LoginBtn = styled.button``;
 const SignupBtn = styled.button``;
